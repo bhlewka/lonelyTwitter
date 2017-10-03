7feb06f26b4ee1c22b5bf257f4fdc9ada3232130
ta301fall2017 <ta301fall2017@gmail.com>
2017-10-03 17:35:04 -0600

    ???

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -1,11 +1,26 @@
+
+
+/**
+ * Lonely Twitter
+ *
+ * Version 1.0
+ *
+ * September 26th, 2017
+ *
+ * Copyright 2017 Team X. CMPUT 301 Blah Blah Blah the text is too small and I cannot read it
+ */
+
 package ca.ualberta.cs.lonelytwitter;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -19,12 +34,21 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+/**
+ * @author bhlewka
+ * @version 1.0
+ */
 public class LonelyTwitterActivity extends Activity {
 
 	private static final String FILENAME = "file.sav";
 	private EditText bodyText;
 	private ListView oldTweetsList;
-	
+	private ArrayList<Tweet> tweetList;
+	private ArrayAdapter<Tweet> adapter;
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -42,73 +66,79 @@ public class LonelyTwitterActivity extends Activity {
 				String text = bodyText.getText().toString();
 
 				NormalTweet newTweet = new NormalTweet("Hello");
-				ImportantTweet newTweet2 = new ImportantTweet("hello", new Date());
 
-				newTweet2.getDate();
-
-				try {
-					newTweet.setMessage("Goodbye");
-				} catch (TweetTooLongException e){
-
-				}
-				Log.d("TWEET", newTweet.getMessage());
-
-
-				ArrayList<Tweet> tweetList = new ArrayList<Tweet>();
 				tweetList.add(newTweet);
-				tweetList.add(newTweet2);
 
-				saveInFile(text, new Date(System.currentTimeMillis()));
+                adapter.notifyDataSetChanged();
+
+				saveInFile();
 				//finish();
 
 			}
 		});
 	}
 
+	/**
+	 * Startup initialization
+	 */
 	@Override
 	protected void onStart() {
 		// TODO Auto-generated method stub
 		super.onStart();
-		String[] tweets = loadFromFile();
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-				R.layout.list_item, tweets);
+		loadFromFile();
+		adapter = new ArrayAdapter<Tweet>(this,
+				R.layout.list_item, tweetList);
 		oldTweetsList.setAdapter(adapter);
 	}
 
-	private String[] loadFromFile() {
-		ArrayList<String> tweets = new ArrayList<String>();
+	/**
+	 * Load from file
+	 */
+	private void loadFromFile() {
 		try {
 			FileInputStream fis = openFileInput(FILENAME);
-			BufferedReader in = new BufferedReader(new InputStreamReader(fis));
-			String line = in.readLine();
-			while (line != null) {
-				tweets.add(line);
-				line = in.readLine();
-			}
+            BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+
+            Gson gson = new Gson();
+
+
+            // Taken from <link website>
+            // Date taken
+
+            Type listType = new TypeToken<ArrayList<NormalTweet>>(){}.getType();
+            tweetList = gson.fromJson(in, listType);
 
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			tweetList = new ArrayList<Tweet>();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			throw new RuntimeException();
 		}
-		return tweets.toArray(new String[tweets.size()]);
 	}
-	
-	private void saveInFile(String text, Date date) {
+
+	/**
+	 * Saves tweets onto phone data
+	 */
+	private void saveInFile() {
 		try {
 			FileOutputStream fos = openFileOutput(FILENAME,
-					Context.MODE_APPEND);
-			fos.write(new String(date.toString() + " | " + text)
-					.getBytes());
-			fos.close();
+					Context.MODE_PRIVATE);
+
+			BufferedWriter out = new BufferedWriter(new OutputStreamWriter(fos));
+
+			Gson gson = new Gson();
+			gson.toJson(tweetList, out);
+            out.flush();
+            fos.close();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+            throw new RuntimeException();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+            throw new RuntimeException();
 		}
 	}
 }
--- app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
@@ -1,3 +1,15 @@
+
+
+/*
+ * Tweet
+ *
+ * Version 1
+ *
+ *
+ *
+ * Copyright 2017 Team X. CMPUT 301 Blah Blah Blah the text is too small and I cannot read it
+ */
+
 package ca.ualberta.cs.lonelytwitter;
 
 import java.util.ArrayList;
@@ -5,6 +17,12 @@ import java.util.Date;
 
 /**
  * Created by watts1 on 9/12/17.
+ *
+ * @author watts01
+ * @version 1.0
+ * @see NormalTweet
+ * @see ImportantTweet
+ * @see Tweetable
  */
 
 public abstract class Tweet {
@@ -19,6 +37,13 @@ public abstract class Tweet {
         this.date = new Date();
     }
 
+    /**
+     *tweets a message
+     *
+     * @param message tweet message
+     * @param date tweet date
+     */
+
     public Tweet(String message, Date date) {
         this.message = message;
         this.date = date;
@@ -27,7 +52,12 @@ public abstract class Tweet {
     public abstract Boolean isImportant();
 
 
-
+    /**
+     * sets tweet message
+     *
+     * @param message message
+     * @throws TweetTooLongException tweet too long
+     */
 
 
     public void setMessage(String message) throws TweetTooLongException{
@@ -38,15 +68,42 @@ public abstract class Tweet {
         }
     }
 
+    /**
+     * returns message
+     *
+     * @return message
+     */
     public String getMessage(){
         return this.message;
     }
 
+    /**
+     * get date
+     *
+     * @return date
+     */
     public Date getDate() {
         return date;
     }
 
+    /**
+     * sets the date
+     *
+     * @param date date
+     */
+
     public void setDate(Date date) {
         this.date = date;
+    }
+
+    /**
+     * returns the date
+     *
+     * @return date
+     */
+
+    @Override
+    public String toString(){
+        return date.toString();
     }
 }
