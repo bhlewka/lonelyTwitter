6e90ddc4b98cff3a344fabeee0b1fd4620111dc9
ta301fall2017 <ta301fall2017@gmail.com>
2017-10-10 18:21:51 -0600

    lab  5

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
@@ -1,8 +1,71 @@
 package ca.ualberta.cs.lonelytwitter;
 
+import android.test.ActivityInstrumentationTestCase2;
+
 /**
  * Created by bhlewka on 10/10/17.
  */
 
-public class TweetListTest {
+// Every method should start with test, otherwise they will not run lol
+public class TweetListTest extends ActivityInstrumentationTestCase2 {
+
+    public TweetListTest() {
+        super(LonelyTwitterActivity.class);
+    }
+
+    public void testAddTweet() {
+
+        TweetList tweets = new TweetList();
+
+        Tweet myTweet = new NormalTweet("my tweet");
+
+        tweets.addTweet(myTweet);
+
+        assertTrue(tweets.hasTweets(myTweet));
+
+    }
+
+    public void testDeleteTweet(){
+
+        TweetList tweets = new TweetList();
+
+        Tweet myTweet = new NormalTweet("my tweet");
+
+        tweets.addTweet(myTweet);
+
+        tweets.deleteTweet(myTweet);
+
+        assertFalse(tweets.hasTweets(myTweet));
+    }
+
+    public void testHasTweet(){
+
+        TweetList tweets = new TweetList();
+
+        Tweet myTweet = new NormalTweet("my tweet");
+
+        assertFalse("Has the tweet", tweets.hasTweets(myTweet));
+
+        tweets.addTweet(myTweet);
+
+        assertTrue("Does not have the tweet", tweets.hasTweets(myTweet));
+
+    }
+
+    public void testGetTweet(){
+
+        TweetList tweets = new TweetList();
+
+        Tweet myTweet = new NormalTweet("my tweet");
+
+        tweets.addTweet(myTweet);
+
+        Tweet returnedTweet = tweets.getTweet(0);
+
+        assertEquals(returnedTweet.getMessage(), myTweet.getMessage());
+
+        assertEquals(returnedTweet.getDate(), myTweet.getDate());
+        //Get tweet returns a tweet;
+
+    }
 }
--- app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
@@ -1,8 +1,65 @@
 package ca.ualberta.cs.lonelytwitter;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 /**
  * Created by bhlewka on 10/10/17.
  */
 
+//addTweet() -- should throw an IllegalArgumentException when one tries to add a duplicate tweet || done
+//getTweets() -- sould return a list of tweets in chronological order || done
+//hasTweets() -- should return true if there is a tweet that equals() another tweet || done
+//getCount() -- should accurately count up the tweets ||  done
+
+
 public class TweetList {
+
+    public TweetList(ArrayList<Tweet> tweets) {
+        this.tweets = tweets;
+    }
+
+    private ArrayList<Tweet> tweets = new ArrayList<Tweet>();
+
+    // add tweet toa list of tweets
+    public void addTweet(Tweet tweet){
+        if (this.hasTweets(tweet)){
+            throw new IllegalArgumentException();
+        }
+        tweets.add(tweet);
+    }
+
+    // delete tweet from a list of tweets
+    public void deleteTweet(Tweet tweet){
+        tweets.remove(tweet);
+    }
+
+    public boolean hasTweets(Tweet tweet){
+        return tweets.contains(tweet);
+    }
+
+    public Tweet getTweet(int index) {
+        return tweets.get(index);
+    }
+    public int getCount(){
+
+        return tweets.size();
+    }
+
+    public TweetList getTweets(){
+
+        ArrayList<Tweet> sorted = tweets;
+
+        Collections.sort(sorted, new Comparator<Tweet>() {
+            public int compare(Tweet lhs, Tweet rhs) {
+                return lhs.getDate().compareTo(rhs.getDate());
+            }
+        });
+
+        // From https://stackoverflow.com/questions/32160742/how-to-sort-arraylistobject-in-ascending-order-android
+
+
+        return new TweetList(sorted);
+    }
 }
